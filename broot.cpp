49f7a87976f598643872ce8c10b0288b1c7701f7
Karolina <karolina.laskowska19@gmail.com>
2021-12-17 22:07:58 +0100

    gotowy broot

--- broot.cpp
+++ broot.cpp
@@ -5,20 +5,31 @@
 void polecenie_O(std::string *s, int j, int k) {
     size_t len = k - j + 1;
     std::string fragment = s->substr(j - 1, len);
-    std::cout << fragment << '\n';
     std::reverse(fragment.begin(), fragment.end());
     s->replace(j - 1, len, fragment);
 }
 void polecenie_P(std::string *s, int j, int k, int l) {
     size_t len = k - j + 1;
     std::string fragment = s->substr(j - 1, len);
-    std::cout << fragment << "\n";
     s->replace(j - 1, k - j + 1, "");
-    std::cout << *s <<"\n";
     s->replace(l - 1, 0, fragment);
 }
-void polecenie_N(std::string &s, int n, int j, int k) {
-    ;
+void polecenie_N(std::string &s, int j, int k) {
+    int maximum = 1;
+    int licznik = 1;
+    char poprzednia = s[j - 1];
+    for (int i = j; i < k; i++) {
+        if (poprzednia == s[i]) {
+            licznik++;
+        }
+        else {
+            poprzednia = s[i];
+            maximum = std::max(maximum, licznik);
+            licznik = 1;
+        }
+    }
+    maximum = std::max(maximum, licznik);
+    std::cout << maximum << '\n';
 }
 
 int main() {
@@ -42,10 +53,8 @@ int main() {
                 polecenie_P(&s, j, k, l);
                 break;
             case 'N':
-                polecenie_N(s, n, j, k);
+                polecenie_N(s, j, k);
         }
     }
-    std::cout << s;
-
     return 0;
 }
